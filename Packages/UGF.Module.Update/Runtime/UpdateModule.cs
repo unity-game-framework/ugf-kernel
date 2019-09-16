cd3106828c7a72b8ce0827839b8dd6ebefb84686
Alexander <vorobyovvorobyov@gmail.com>
2019-09-16 22:43:47 +0300

    Update UGF.Module.Update

--- Packages/UGF.Module.Update/Runtime/UpdateModule.cs
+++ Packages/UGF.Module.Update/Runtime/UpdateModule.cs
@@ -1,6 +1,79 @@
+using UGF.Application.Runtime;
+using UGF.Module.Update.Runtime.Handlers;
+using UGF.Update.Runtime;
+using UnityEngine.Experimental.LowLevel;
+using UnityEngine.Profiling;
+using PlayerLoops = UnityEngine.Experimental.PlayerLoop;
+
 namespace UGF.Module.Update.Runtime
 {
-    public class UpdateModule
+    public class UpdateModule : ApplicationModuleBase, IUpdateModule
     {
+        public IUpdateCollection<IPreUpdateHandler> PreUpdate { get { return m_preUpdate; } }
+        public IUpdateCollection<IUpdateHandler> Update { get { return m_update; } }
+        public IUpdateCollection<IFixedUpdateHandler> FixedUpdate { get { return m_fixedUpdate; } }
+        public IUpdateCollection<IPostLateUpdateHandler> PostLateUpdate { get { return m_postLateUpdate; } }
+
+        private readonly UpdateCollectionFunction<IPreUpdateHandler> m_preUpdate = new UpdateCollectionFunction<IPreUpdateHandler>(handler => handler.PreUpdate());
+        private readonly UpdateCollection<IUpdateHandler> m_update = new UpdateCollection<IUpdateHandler>();
+        private readonly UpdateCollectionFunction<IFixedUpdateHandler> m_fixedUpdate = new UpdateCollectionFunction<IFixedUpdateHandler>(handler => handler.OnFixedUpdate());
+        private readonly UpdateCollectionFunction<IPostLateUpdateHandler> m_postLateUpdate = new UpdateCollectionFunction<IPostLateUpdateHandler>(handler => handler.OnPostLateUpdate());
+
+        protected override void OnInitialize()
+        {
+            base.OnInitialize();
+
+            PlayerLoopSystem playerLoop = PlayerLoop.GetDefaultPlayerLoop();
+
+            UpdateUtility.TryAddUpdateFunction(playerLoop, typeof(PlayerLoops.PreUpdate), OnPreUpdate);
+            UpdateUtility.TryAddUpdateFunction(playerLoop, typeof(PlayerLoops.Update), OnUpdate);
+            UpdateUtility.TryAddUpdateFunction(playerLoop, typeof(PlayerLoops.FixedUpdate), OnFixedUpdate);
+            UpdateUtility.TryAddUpdateFunction(playerLoop, typeof(PlayerLoops.PostLateUpdate), OnPostLateUpdate);
+
+            PlayerLoop.SetPlayerLoop(playerLoop);
+        }
+
+        protected override void OnUninitialize()
+        {
+            base.OnUninitialize();
+
+            PlayerLoop.SetPlayerLoop(PlayerLoop.GetDefaultPlayerLoop());
+        }
+
+        private void OnPreUpdate()
+        {
+            Profiler.BeginSample("UpdateModule.OnPreUpdate");
+
+            m_preUpdate.ApplyQueueAndUpdate();
+
+            Profiler.EndSample();
+        }
+
+        private void OnUpdate()
+        {
+            Profiler.BeginSample("UpdateModule.OnUpdate");
+
+            m_update.ApplyQueueAndUpdate();
+
+            Profiler.EndSample();
+        }
+
+        private void OnFixedUpdate()
+        {
+            Profiler.BeginSample("UpdateModule.OnFixedUpdate");
+
+            m_fixedUpdate.ApplyQueueAndUpdate();
+
+            Profiler.EndSample();
+        }
+
+        private void OnPostLateUpdate()
+        {
+            Profiler.BeginSample("UpdateModule.OnPostLateUpdate");
+
+            m_postLateUpdate.ApplyQueueAndUpdate();
+
+            Profiler.EndSample();
+        }
     }
 }
