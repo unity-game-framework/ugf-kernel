56f614ea7840e2f486276cb8fd4e33b4a66ed0dc
Alexander <vorobyovvorobyov@gmail.com>
2019-09-20 22:59:44 +0300

    Add SceneModuleBuilder.asset

--- Packages/UGF.Module.Scenes/Runtime/SceneModuleBuilderAsset.cs
+++ Packages/UGF.Module.Scenes/Runtime/SceneModuleBuilderAsset.cs
@@ -2,9 +2,11 @@ using UGF.Application.Runtime;
 using UGF.Description.Runtime;
 using UGF.Module.Coroutines.Runtime;
 using UGF.Module.Runtime;
+using UnityEngine;
 
 namespace UGF.Module.Scenes.Runtime
 {
+    [CreateAssetMenu(menuName = "UGF/Module.Scenes/SceneModuleBuilderAsset", order = 2000)]
     public class SceneModuleBuilderAsset : ModuleBuilderAsset<ISceneModule>
     {
         protected override IApplicationModule OnBuild(IApplication application, IModuleBuildArguments<IDescription> arguments)
