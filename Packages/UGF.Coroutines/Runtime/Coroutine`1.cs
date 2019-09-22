fdf026c09c72538efd5127624272050dab3a59bb
Alexander <vorobyovvorobyov@gmail.com>
2019-09-22 17:10:51 +0300

    Update Coroutine`1.cs

--- Packages/UGF.Coroutines/Runtime/Coroutine`1.cs
+++ Packages/UGF.Coroutines/Runtime/Coroutine`1.cs
@@ -22,7 +22,7 @@ namespace UGF.Coroutines.Runtime
             protected set { m_result = value; }
         }
 
-        public bool HasResult { get { return typeof(TResult).IsValueType || EqualityComparer<TResult>.Default.Equals(m_result, default); } }
+        public bool HasResult { get { return typeof(TResult).IsValueType || !EqualityComparer<TResult>.Default.Equals(m_result, default); } }
 
         public event CoroutineHandler<TResult> Completed;
 
