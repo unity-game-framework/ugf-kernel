e996a051fadaccb8250c31058f365a14922a67f5
Alexander <vorobyovvorobyov@gmail.com>
2019-10-08 23:27:08 +0300

    Update packages

--- Packages/UGF.Module.Unity/Runtime/IUnityModule.cs
+++
@@ -1,9 +0,0 @@
-using UGF.Application.Runtime;
-
-namespace UGF.Module.Unity.Runtime
-{
-    public interface IUnityModule : IApplicationModule
-    {
-        IUnityResources Resources { get; }
-    }
-}
